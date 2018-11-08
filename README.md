e28a3aebff8d2e84112ce8cf96587e126f967a07
TaufeeqR <taurajap@gmail.com>
2018-11-08 09:26:07 +0200

    Updated Readme and php file.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# php
-Intro into php
+# PHP
+
+## What is php?
+
+```
+PHP stands for Hypertext Preprocessor (no, the acronym doesn't follow the name). It is an open source, server-side, scripting language used for the development of web applications. By scripting language, it means a program that is script-based (lines of code) written for the automation of tasks.
+```
+
+## Applications used
+
+```
+- Wamp
+- Visual Studio
+- Chrome
+```
+
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP_variable_functions_challenge</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+
+<?php
+
+name = "  Bob  ";
+
+age = "thirty five";
+
+$job = plumber;
+
+$carMake = " Toyota";
+
+$carColour = "RED";
+
+$happy = true
+
+$time = "this many";
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+</body>
+</html>
